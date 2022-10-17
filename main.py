57cf27225d0d843f0366a09f6d0ddf6dbc0d59ed
Saurabh <sbj.amb@gmail.com>
2022-10-17 15:20:01 +0530

    Download button disables till download completes

--- main.py
+++ main.py
@@ -7,46 +7,53 @@ import yt_dlp
 from moviepy.editor import VideoFileClip, AudioFileClip
 
 
-def get_original_video(label, video_info, folder):
-    if os.path.exists(f"{folder}/{video_info['title']}.mp4"):
-        os.remove(f"{folder}/{video_info['title']}.mp4")
+def get_original_video(label, info, folder):
+    if os.path.exists(f"{info['title']}.mp4"):
+        os.remove(f"{info['title']}.mp4")
     options = {
         'format': '136/137/mp4/bestvideo,140/m4a/bestaudio',
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',
         }],
-        # 'outtmpl': f"{folder}{video_info['title']}.mp4",
+        # 'outtmpl': f"{video_info['title']}",
         'keepvideo': True,
         'keepaudio': True,
     }
 
+    # try:
     with yt_dlp.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
+        ydl.download([info['webpage_url']])
+    # except Exception as e:
+    #     print(e)
 
 
-def pitch_shift(label, folder, title, step):
+def pitch_shift(label, info, step):
     label.config(text=f"Shifting pitch of the audio by {step} steps", fg="blue")
-    audio = folder + title + '.f140.m4a'
+    # audio = folder + title + '.f140.m4a'
+    audio = f"{info['title']}[{info['id']}].m4a"
     sound = pydub.AudioSegment.from_file(audio)
     sound.export('out.wav', format='wav')
     y, sr = sf.read('out.wav')
     y_shift = pyrubberband.pitch_shift(y, sr=sr, n_steps=step)
     sf.write('outshifted.wav', y_shift, sr, format="wav")
 
-    os.remove(folder + title + '.f140.m4a')
+    # os.remove(folder + title + '.f140.m4a')
+    os.remove(f"{info['title']}[{info['id']}].m4a")
     os.remove('out.wav')
 
 
-def scale_changed_video(label, folder, title, scale):
+def scale_changed_video(label, info, folder, scale):
     label.config(text="Merging video and pitch shifted audio", fg="blue")
-    video = VideoFileClip(folder + title + '.f137.mp4')
+    # video = VideoFileClip(folder + title + '.f137.mp4')
+    video = VideoFileClip(f"{info['title']}[{info['id']}].mp4")
     audio = AudioFileClip('outshifted.wav')
 
     videoclip = video.set_audio(audio)
-    videoclip.write_videofile(folder + title + f'_scale_changed_{int(scale)}.mp4')
+    videoclip.write_videofile(folder + info['title'] + f'_scale_changed_{int(scale)}.mp4')
 
-    os.remove(folder + title + '.f137.mp4')
+    # os.remove(folder + title + '.f137.mp4')
+    os.remove(f"{info['title']}[{info['id']}].mp4")
     os.remove('outshifted.wav')
 
 
@@ -67,8 +74,8 @@ def main(label, button, url, scale=0, output_folder='~'):
             get_original_video(label, info, output_folder)
             if scale:
                 pass
-                pitch_shift(label, output_folder, info['title'], int(scale))
-                scale_changed_video(label, output_folder, info['title'], scale)
+                pitch_shift(label, info, int(scale))
+                scale_changed_video(label, info, output_folder, scale)
             label.config(text=f"Download Complete!!! Videos stored to {output_folder}", fg="green")
         except Exception as e:
             print(e)
